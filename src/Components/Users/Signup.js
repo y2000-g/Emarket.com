4e858c1cfe5f01668fce15235bf8b73e114ce3c9
Yashita Gole <yashitagole2019@gmail.com>
2024-11-10 18:20:38 +0530

    Added signup page

--- src/Components/Users/Signup.js
+++ src/Components/Users/Signup.js
@@ -3,7 +3,12 @@ function Signup() {
 
     return (
       <div>
-        Hello Emarket
+        <fieldset>
+          <form>
+            <h1>Signup</h1>
+            <label>Name :<input type="Text"></input></label>
+          </form>
+        </fieldset>
       </div>
     );
   }
